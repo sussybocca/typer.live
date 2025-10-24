bf1358d6fc7e23b02d06ad27a3dc6a701a219d6d
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 07:42:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TypingProvider } from './context/TypingContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Lesson from './pages/Lesson';
+import Progress from './pages/Progress';
 
+function App() {
+  return (
+    <TypingProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lesson/:id" element={<Lesson />} />
+          <Route path="/progress" element={<Progress />} />
+        </Routes>
+      </Router>
+    </TypingProvider>
+  );
+}
+
+export default App;

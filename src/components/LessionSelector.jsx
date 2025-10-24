7af0b9986fd5ababbb0640159dce9370f13f503a
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 07:36:13 -0500

    Update LessionSelector.jsx

---
+++ src/components/LessionSelector.jsx
@@ -0,0 +1,22 @@
+import lessons from '../data/lessons.json';
+import { Link } from 'react-router-dom';
+
+export default function LessonSelector() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Typing Lessons</h1>
+      <ul>
+        {lessons.map((lesson) => (
+          <li key={lesson.id} className="mb-2">
+            <Link
+              to={`/lesson/${lesson.id}`}
+              className="text-blue-600 hover:underline"
+            >
+              {lesson.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
